da4898628d0570435bfb9713ab64cdcc7224ba07
ArabuliGiorgi <qartveli.giorgi.2003@gmail.com>
2024-07-16 17:12:35 +0400

    fixed timer

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ let hoursEl = document.getElementById("hours");
 let minutesEl = document.getElementById("minutes");
 let secondsEl = document.getElementById("seconds");
 
-let days = 8;
-let hours = 23;
-let minutes = 55;
-let seconds = 41;
+let launchTime = new Date(2024, 7, 15, 0, 0, 0);
+let current = new Date();
+let dif = Math.floor((launchTime - current)/1000);
+
+let seconds = 0, minutes = 0, hours = 0, days = 0;
+if(dif > 0){
+    seconds = dif%60;
+    minutes = Math.floor(dif/60)%60;
+    hours = Math.floor(dif/3600)%60;
+    days = Math.floor(dif/3600/24);
+}
 
 function format(num){
     let str = num.toString();
@@ -20,22 +27,17 @@ secondsEl.textContent = format(seconds);
 
 let interv = null;
 function timer(){
-    seconds--;
-    if(days === 0 && hours === 0 && minutes === 0 && seconds === 0){
+    if(days <= 0 && hours <= 0 && minutes <= 0 && seconds <= 0){
         clearInterval(interv);
+        return;
     }
-    if(seconds < 0){
-        seconds = 59;
-        minutes--;
-        if(minutes < 0){
-            minutes = 59;
-            hours--;
-            if(hours < 0){
-                hours = 23;
-                days--;
-            }
-        }
-    }
+
+    current = new Date();
+    dif = Math.floor((launchTime - current)/1000);
+    seconds = dif%60;
+    minutes = Math.floor(dif/60)%60;
+    hours = Math.floor(dif/3600)%60;
+    days = Math.floor(dif/3600/24);
 
     daysEl.textContent = format(days);
     hoursEl.textContent = format(hours);
@@ -44,4 +46,4 @@ function timer(){
 }
 interv = setInterval(()=> {
     timer();
-}, 1000);
+}, 50);
